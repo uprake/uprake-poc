63980075acaa02b4994114d626387f5ed753f0e6
Kaustubh Agrawal <kaustubhkagrawal@gmail.com>
2022-02-23 11:19:49 +0530

    React Router Setup done

--- apps/landing/src/app/App.tsx
+++ apps/landing/src/app/App.tsx
@@ -1,6 +1,5 @@
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
-
-import { Route, Link } from 'react-router-dom';
 
 export function App() {
   return (
@@ -23,23 +22,25 @@ export function App() {
           </li>
         </ul>
       </div>
-      <Route
-        path="/"
-        element={
-          <div>
-            This is the generated root route.{' '}
-            <Link to="/page-2">Click here for page 2.</Link>
-          </div>
-        }
-      />
-      <Route
-        path="/page-2"
-        element={
-          <div>
-            <Link to="/">Click here to go back to root page.</Link>
-          </div>
-        }
-      />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              This is the generated root route.{' '}
+              <Link to="/page-2">Click here for page 2.</Link>
+            </div>
+          }
+        />
+        <Route
+          path="/page-2"
+          element={
+            <div>
+              <Link to="/">Click here to go back to root page.</Link>
+            </div>
+          }
+        />
+      </Routes>
       {/* END: routes */}
     </>
   );
