d7c331f3934e252a19198997e89f97375c8bac36
Vaishnav <vaishanvkagrawal2.0@gmail.com>
2022-03-24 19:18:25 +0530

    basic styling of container

--- apps/web-ext/src/contentScripts/index.tsx
+++ apps/web-ext/src/contentScripts/index.tsx
@@ -1,34 +1,41 @@
 /* eslint-disable no-console */
-import React from "react";
-import ReactDOM from "react-dom";
-import { onMessage } from "webext-bridge";
-import browser from "webextension-polyfill";
-import { ContentApp } from "./views/ContentApp";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { setup, tw } from 'twind';
+import { onMessage } from 'webext-bridge';
+import browser from 'webextension-polyfill';
+import { ContentApp } from './views/ContentApp';
 
+setup({
+  preflight: false,
+});
 // Firefox `browser.tabs.executeScript()` requires scripts return a primitive value
 (() => {
-  console.info("[vitesse-webext] Hello world from content script");
+  console.info('[vitesse-webext] Hello world from content script');
 
   // communication example: send previous tab title from background page
-  onMessage("tab-prev", ({ data }) => {
+  onMessage('tab-prev', ({ data }) => {
     console.log(`[vitesse-webext] Navigate from page "${data}"`);
   });
 
+  const style = 'right-0 left-0 top-0 fixed bg-blue-300 z-[10000000] ';
+
   // mount component to context window
-  const container = document.createElement("div");
-  const root = document.createElement("div");
-  const styleEl = document.createElement("link");
+  const container = document.createElement('div');
+  container.setAttribute('class', tw`${style}`);
+  const root = document.createElement('div');
+  const styleEl = document.createElement('link');
   const shadowDOM =
-    container.attachShadow?.({ mode: __DEV__ ? "open" : "closed" }) ||
+    container.attachShadow?.({ mode: __DEV__ ? 'open' : 'closed' }) ||
     container;
-  styleEl.setAttribute("rel", "stylesheet");
+  styleEl.setAttribute('rel', 'stylesheet');
   styleEl.setAttribute(
-    "href",
-    browser.runtime.getURL("dist/contentScripts/style.css")
+    'href',
+    browser.runtime.getURL('dist/contentScripts/style.css')
   );
   shadowDOM.appendChild(styleEl);
   shadowDOM.appendChild(root);
-  document.body.appendChild(container);
+  document.body.prepend(container);
 
   ReactDOM.render(
     <React.StrictMode>
