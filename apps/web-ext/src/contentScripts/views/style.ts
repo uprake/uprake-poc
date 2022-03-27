96c8518425b3e6ee48a8b8f00575e6466ed74873
Kaustubh Agrawal <kaustubhkagrawal@gmail.com>
2022-03-27 18:26:59 +0530

    adding tailwindcss to project

--- apps/web-ext/src/contentScripts/views/style.ts
+++ apps/web-ext/src/contentScripts/views/style.ts
@@ -2,7 +2,7 @@ import { CSSProperties } from 'react';
 
 export const style: Record<string, CSSProperties> = {
   trackPad: {
-    background: 'white',
+    // background: 'white',
     fontSize: '16px',
     // minHeight: '300px',
     // minWidth: '300px',
