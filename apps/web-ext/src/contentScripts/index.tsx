e01db42e59e520857a7a7877510644612f5e57cd
Vaishnav <vaishanvkagrawal2.0@gmail.com>
2022-05-10 17:44:03 +0530

    shadow element removed

--- apps/web-ext/src/contentScripts/index.tsx
+++ apps/web-ext/src/contentScripts/index.tsx
@@ -1,34 +1,54 @@
 /* eslint-disable no-console */
-import React from "react";
-import ReactDOM from "react-dom";
-import { onMessage } from "webext-bridge";
-import browser from "webextension-polyfill";
-import { ContentApp } from "./views/ContentApp";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { setup, tw } from 'twind';
+import { onMessage } from 'webext-bridge';
+import { ContentApp } from './views/ContentApp';
 
 // Firefox `browser.tabs.executeScript()` requires scripts return a primitive value
+setup({
+  preflight: false,
+  prefix: true,
+  // hash: (a: string) => 'up-' + hash(a),
+});
 (() => {
-  console.info("[vitesse-webext] Hello world from content script");
+  console.info('[vitesse-webext] Hello world from content script');
 
   // communication example: send previous tab title from background page
-  onMessage("tab-prev", ({ data }) => {
+  onMessage('tab-prev', ({ data }) => {
     console.log(`[vitesse-webext] Navigate from page "${data}"`);
   });
 
+  const style = ' left-0 top-0 fixed  z-[10000000] ';
+
   // mount component to context window
-  const container = document.createElement("div");
-  const root = document.createElement("div");
-  const styleEl = document.createElement("link");
-  const shadowDOM =
-    container.attachShadow?.({ mode: __DEV__ ? "open" : "closed" }) ||
-    container;
-  styleEl.setAttribute("rel", "stylesheet");
-  styleEl.setAttribute(
-    "href",
-    browser.runtime.getURL("dist/contentScripts/style.css")
-  );
-  shadowDOM.appendChild(styleEl);
-  shadowDOM.appendChild(root);
-  document.body.appendChild(container);
+  // const container = document.createElement("div");
+  // const root = document.createElement("div");
+  // const styleEl = document.createElement("link");
+  // const shadowDOM =
+  //   container.attachShadow?.({ mode: __DEV__ ? "open" : "closed" }) ||
+  //   container;
+  // styleEl.setAttribute("rel", "stylesheet");
+  // styleEl.setAttribute(
+  //   "href",
+  //   browser.runtime.getURL("dist/contentScripts/style.css")
+  // );
+  // shadowDOM.appendChild(styleEl);
+  // shadowDOM.appendChild(root);
+  // document.body.appendChild(container);
+
+  // ReactDOM.render(
+  //   <React.StrictMode>
+  //     <ContentApp />
+  //   </React.StrictMode>,
+  //   root
+  // );
+  // mount component to context window
+  const container = document.createElement('div');
+  container.setAttribute('class', tw`${style}`);
+  const root = document.createElement('div');
+  container.appendChild(root);
+  document.body.prepend(container);
 
   ReactDOM.render(
     <React.StrictMode>
--- apps/web-ext/src/contentScripts/views/ContentApp.tsx
+++ apps/web-ext/src/contentScripts/views/ContentApp.tsx
@@ -1,3 +1,5 @@
+import { tw } from 'twind';
+
 export const ContentApp = () => {
-  return <div>Hey!</div>;
+  return <div className={tw`text-[16px]`}>ContentApp</div>;
 };
